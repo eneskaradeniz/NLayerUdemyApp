719a0d8bac388b402a5bc67a75ac4e40f8de575e
enesk <enesk@ENES>
2023-10-16 16:44:37 +0300

    Add project files.

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext : DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        // Öğrenme açısından burası açık yoksa products üzerinden işlemler yapılmalı
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+    }
+}
