d9e7a3074f1ab54cfccd96abf54ea9aab06a4662
Enes Karadeniz <eneskaradenz@outlook.com>
2023-10-17 11:20:01 +0300

    FluentValidation Library

---
+++ NLayer.Service/Validations/ProductDtoValidatior.cs
@@ -0,0 +1,31 @@
+﻿
+using FluentValidation;
+using NLayer.Core.DTOs;
+
+namespace NLayer.Service.Validations
+{
+    public class ProductDtoValidatior : AbstractValidator<ProductDto>
+    {
+
+        public ProductDtoValidatior()
+        {
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("{PropertyName} is required")
+                .NotEmpty()
+                .WithMessage("{PropertyName} is required");
+
+            RuleFor(x => x.Price)
+                .InclusiveBetween(1, int.MaxValue)
+                .WithMessage("{PropertyName} must be grater 0");
+
+            RuleFor(x => x.Stock)
+                .InclusiveBetween(1, int.MaxValue)
+                .WithMessage("{PropertyName} must be grater 0");
+
+            RuleFor(x => x.CategoryId)
+                .InclusiveBetween(1, int.MaxValue)
+                .WithMessage("{PropertyName} must be grater 0");
+        }
+    }
+}
