cbb78adaabd7e76118035142372a788705b7775b
Enes Karadeniz <eneskaradenz@outlook.com>
2023-10-17 10:20:42 +0300

    ProductsWithCategory Endpoint

---
+++ NLayer.Repository/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Models;
+using NLayer.Core.Repositories;
+
+namespace NLayer.Repository.Repositories
+{
+    public class ProductRepository : GenericRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext context) : base(context)
+        {
+
+        }
+
+        public async Task<List<Product>> GetProductsWithCategory()
+        {
+            // Eager loading => product çekerne categoryleride al dedik
+            return await _context.Products.Include(x=>x.Category).ToListAsync();
+        }
+    }
+}
