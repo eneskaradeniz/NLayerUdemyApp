2d3d0147ee5ed430667e8605053077d9a4dd419d
Enes Karadeniz <eneskaradenz@outlook.com>
2023-10-18 11:54:31 +0300

    ProductsController Update-1

--- NLayer.Web/Controllers/ProductsController.cs
+++ NLayer.Web/Controllers/ProductsController.cs
@@ -9,20 +9,20 @@ namespace NLayer.Web.Controllers
 {
     public class ProductsController : Controller
     {
-        private readonly IProductService _service;
+        private readonly IProductService _productService;
         private readonly ICategoryService _categoryService;
         private readonly IMapper _mapper;
 
-        public ProductsController(IProductService service, ICategoryService categoryService, IMapper mapper)
+        public ProductsController(IProductService productService, ICategoryService categoryService, IMapper mapper)
         {
-            _service = service;
+            _productService = productService;
             _categoryService = categoryService;
             _mapper = mapper;
         }
 
         public async Task<IActionResult> Index()
         {
-            var result = await _service.GetProductsWithCategory();
+            var result = await _productService.GetProductsWithCategory();
             return View(result);
         }
 
@@ -40,7 +40,7 @@ namespace NLayer.Web.Controllers
         {
             if (ModelState.IsValid)
             {
-                await _service.AddAsync(_mapper.Map<Product>(productDto));
+                await _productService.AddAsync(_mapper.Map<Product>(productDto));
                 return RedirectToAction(nameof(Index));
             }
 
@@ -50,5 +50,33 @@ namespace NLayer.Web.Controllers
             ViewBag.categories = new SelectList(categoriesDto, "Id", "Name");
             return View();
         }
-    }
+
+        public async Task<IActionResult> Update(int id)
+        {
+            var product = await _productService.GetByIdAsync(id);
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", product.CategoryId);
+
+            return View(_mapper.Map<ProductDto>(product));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(ProductDto productDto)
+        {
+            if(ModelState.IsValid)
+            {
+                await _productService.UpdateAsync(_mapper.Map<Product>(productDto));
+
+            }
+
+            var categories = await _categoryService.GetAllAsync();
+            var categoriesDto = _mapper.Map<List<CategoryDto>>(categories.ToList());
+
+            ViewBag.categories = new SelectList(categoriesDto, "Id", "Name", productDto.CategoryId);
+
+            return View(productDto);
+        }
 }
